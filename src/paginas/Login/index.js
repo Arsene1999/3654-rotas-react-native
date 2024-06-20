b7904dc0eee6700ba33365fc29620b9a7ce7f386
João Victo Dantas <67969013+Arsene1999@users.noreply.github.com>
2024-06-19 22:23:26 -0300

    fim do projeto

--- src/paginas/Login/index.js
+++ src/paginas/Login/index.js
@@ -36,7 +36,7 @@ export default function Login({ navigation }) {
                             placeholderTextColor={'#BCBCBC'}
                         />
 
-                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Tab')}>
+                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Draw')}>
                             <Text style={styles.textoBotao}>Entrar</Text>
                         </TouchableOpacity>
                     </View>
--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -1,16 +1,83 @@
+import { Image } from 'react-native';
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Image } from 'react-native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Home from '../paginas/Home';
 import Login from '../paginas/Login'
 import ListaPets from '../paginas/ListaPets';
 import Mensagem from '../paginas/Mensagem';
 import Sobre from '../paginas/Sobre';
+import Perfil from '../paginas/Perfil';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerRoutes() {
+    return (
+        <Drawer.Navigator 
+            screenOptions={{
+                drawerStyle: {                
+                    backgroundColor: '#36D6aD',
+                    },
+                drawerLabelStyle: {
+                    color: '#fff',
+                    fontSize: 14,
+                    fontWeight: 400,
+                    lineHeight:20
+                }
+                }
+            }
+        >
+            <Drawer.Screen 
+                name='Lista de Pets' 
+                component={TabRoutes} 
+                options={
+                    {
+                    drawerLabel: 'Pets para adoção',
+                    headerTransparent: true,
+                    title: '',
+                    drawerIcon: 
+                        () => 
+                            <Image 
+                                source={require('../assets/pets.png')} 
+                                style={{width: 24, height:24}}
+                                
+                                />
+                    }
+                }
+            />
+            <Drawer.Screen 
+                name='Perfil' 
+                component={Perfil} 
+                options={
+                    {
+                    drawerLabel: 'Perfil',
+                    headerTransparent: true,
+                    title: '',
+                    drawerIcon: 
+                        () => 
+                            <Image 
+                                source={require('../assets/avatar.png')} 
+                                style={{width: 24, height:24}}
+                                />
+                    }
+                }
+            />
+            <Drawer.Screen 
+                name='Sair' 
+                component={Home}
+                options={{
+                    headerTitle: '',
+                    headerShown: false
+                    }}
+            />
+        </Drawer.Navigator>
+    )
+}
 
 function TabRoutes() {
     return (
@@ -69,7 +136,7 @@ export default function Navigation() {
                     
                 />   
                 <Stack.Screen name="Login" component={Login} />   
-                <Stack.Screen name='Tab' component={TabRoutes} />
+                <Stack.Screen name='Draw' component={DrawerRoutes} />
                 <Stack.Screen name='Sobre' component={Sobre} />
             </Stack.Navigator>
         </NavigationContainer>
